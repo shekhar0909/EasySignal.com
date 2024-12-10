702d1eb218adee0fee1cb352fae9388210bb1ab9
yogeshtaramio <yogeshwar.swami@taram.io>
2024-12-10 17:39:24 +0530

    deploying page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/EasySignal.com/',
   server:{
     open:true,
     host:true,

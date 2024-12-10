daf5ec44f31c041cbf02a6941858a94f9cf1ce52
CodeArtist7071 <65782896+CodeArtist7071@users.noreply.github.com>
2024-12-10 17:49:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://github.com/shekhar0909/EasySignal.com/',
+  base:'/EasySignal.com/',
   server:{
     open:true,
     host:true,

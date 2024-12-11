c535b7633b44d8d1127c5adfaaea32f28a0633f4
shekhar0909 <75560524+shekhar0909@users.noreply.github.com>
2024-12-11 07:55:05 +0000

    login button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin,useGoogleLogin } from "@react-oauth/google";
 
 
 
@@ -10,11 +10,16 @@ export const Navbar = () => {
   const handleError =()=>{
     console.log('login Error')
   }
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse),
+    onError:()=>console.log('Login Failed')
+  });
   return (
     <nav className="container">
       <div className="px-10 flex py-10 justify-between text-white lg:px-20">
       <p className="uppercase text-custom-blue font-bold">Easy Signal</p>
-      <GoogleLogin onSuccess={handleSucess} onError={handleError} text={"signin"} type="standard"/>
+      {/* <GoogleLogin onSuccess={handleSucess} onError={handleError} text={"signin"} type="standard"/> */}
+      <button onClick={()=>login()} type="button">Login</button>
         </div>
     </nav>
   );
